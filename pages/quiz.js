bf80e29a9ebe8a85bd79b59763c6679cec111d1b
Btaft6278 <btaft6278@gmail.com>
2022-09-19 19:56:05 -0400

    fixed mui button hover bug

--- pages/quiz.js
+++ pages/quiz.js
@@ -178,7 +178,7 @@ function StartQuiz() {
               <h1 >Congratulations</h1>
               <h2>Your score is <span className="done-score">{finalScore}</span></h2>
               <Link href='/leaderboard'>
-                <a><span className="final-btn">Leaderboard</span></a>
+                <a><span className="final-btn">LEADERBOARD</span></a>
               </Link>
             </div>
 
