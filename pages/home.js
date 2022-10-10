ca5b085d21cc7414108153eac08052182e5bff91
Btaft6278 <btaft6278@gmail.com>
2022-10-10 19:30:11 -0400

    eod

--- pages/home.js
+++ pages/home.js
@@ -20,24 +20,102 @@ function SelectionPage() {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        var z = document.getElementById("option")
-        // var middle = window.innerHeight / 2;
-        var middle = z.clientHeight / 2;
-        console.log("middle window", middle)
-        var myDiv = document.getElementById("myDiv");
-        var myDivLocation = myDiv.getBoundingClientRect()
-        console.log("myDiv location", myDivLocation)
-        if(myDivLocation.top > middle && myDivLocation.top < (middle+myDivLocation.height - 75)){
-            myDiv.classList.add("grow")
-        } else (
-            myDiv.classList.remove("grow")
-        )
-        const onScroll = () => setCount(count + 1);
-        // clean up code
-        z.removeEventListener('scroll', onScroll);
-        z.addEventListener('scroll', onScroll, { passive: true });
+        // var z = document.getElementById("option")
+        // // var middle = window.innerHeight / 2;
+        // var middle = z.clientHeight / 2;
+        // console.log("middle window", middle)
+        // var myDiv = document.getElementById("myDiv");
+        // var myDivLocation = myDiv.getBoundingClientRect()
+        // console.log("myDiv location", myDivLocation)
+        // if(myDivLocation.top > middle && myDivLocation.top < (middle+myDivLocation.height)){
+        //     myDiv.classList.add("grow")
+        // } else (
+        //     myDiv.classList.remove("grow")
+        // )
+        // const onScroll = () => setCount(count + 1);
+        // // clean up code
+        // z.removeEventListener('scroll', onScroll);
+        // z.addEventListener('scroll', onScroll, { passive: true });
         
-        return () => z.removeEventListener('scroll', onScroll);
+        // return () => z.removeEventListener('scroll', onScroll);
+        var doc = window.document,
+  context = doc.querySelector('.js-loop'),
+  clones = context.querySelectorAll('.is-clone'),
+  disableScroll = false,
+  scrollHeight = 0,
+  scrollPos = 0,
+  clonesHeight = 0,
+  i = 0;
+
+function getScrollPos () {
+  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
+}
+
+function setScrollPos (pos) {
+  context.scrollTop = pos;
+}
+
+function getClonesHeight () {
+  clonesHeight = 0;
+
+  for (i = 0; i < clones.length; i += 1) {
+    clonesHeight = clonesHeight + clones[i].offsetHeight;
+  }
+
+  return clonesHeight;
+}
+
+function reCalc () {
+  scrollPos = getScrollPos();
+  scrollHeight = context.scrollHeight;
+  clonesHeight = getClonesHeight();
+
+  if (scrollPos <= 0) {
+    setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
+  }
+}
+
+function scrollUpdate () {
+  if (!disableScroll) {
+    scrollPos = getScrollPos();
+
+    if (clonesHeight + scrollPos >= scrollHeight) {
+      // Scroll to the top when you’ve reached the bottom
+      setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
+      disableScroll = true;
+    } else if (scrollPos <= 0) {
+      // Scroll to the bottom when you reach the top
+      setScrollPos(scrollHeight - clonesHeight);
+      disableScroll = true;
+    }
+  }
+
+  if (disableScroll) {
+    // Disable scroll-jumping for a short time to avoid flickering
+    window.setTimeout(function () {
+      disableScroll = false;
+    }, 40);
+  }
+}
+
+function init () {
+  reCalc();
+  
+  context.addEventListener('scroll', function () {
+    window.requestAnimationFrame(scrollUpdate);
+  }, false);
+
+  window.addEventListener('resize', function () {
+    window.requestAnimationFrame(reCalc);
+  }, false);
+}
+
+if (document.readyState !== 'loading') {
+  init()
+} else {
+  doc.addEventListener('DOMContentLoaded', init, false)
+}
+
         
     }, [count]);
 
@@ -69,10 +147,10 @@ function SelectionPage() {
     //     }
 
         return(
-            <div className="home column" id="option">
+            <div className="home column Loop js-loop" id="option">
             <div className="top-fade"></div>
             
-                            <Box  className="grid-item fading" >
+                            <Box  className="grid-item fading one" >
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -87,7 +165,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading two">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -102,7 +180,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading three">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -117,7 +195,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading" id="myDiv">
+                            <Box className="grid-item fading four" id="myDiv">
                                 <div className="yellow m-auto">
                                     <Link
                                         href={{
@@ -135,7 +213,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading five">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -150,7 +228,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading six">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -165,7 +243,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading seven">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -180,7 +258,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading six">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -195,7 +273,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading five">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -210,7 +288,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading four">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -225,7 +303,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading three">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -240,7 +318,7 @@ function SelectionPage() {
                             </Box>
                         
                        
-                            <Box className="grid-item fading">
+                            <Box className="grid-item fading two">
                                 <Link
                                     href={{
                                         pathname: '/quiz',
@@ -253,6 +331,33 @@ function SelectionPage() {
                                     </div>
                                 </Link>
                             </Box>
+                            <Box  className="grid-item fading one" >
+                                <Link
+                                    href={{
+                                        pathname: '/quiz',
+                                        query: { cat: '9' },
+                                    }}
+                                >
+                                    <div className="yellow m-auto">
+                                        <PsychologyIcon />
+                                        <div>GENERAL</div>
+                                    </div>
+                                </Link>
+                            </Box>
+                            <Box className="grid-item fading two">
+                                <Link
+                                    href={{
+                                        pathname: '/quiz',
+                                        query: { cat: '11' },
+                                    }}
+                                >
+                                    <div className="yellow m-auto">
+                                        <MovieIcon />
+                                        <div>MOVIES</div>
+                                    </div>
+                                </Link>
+                            </Box>
+                        
                             <div className="bottom-fade"></div>
             </div>
        )
