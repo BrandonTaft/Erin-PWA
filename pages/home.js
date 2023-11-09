8dafd301cac674dd7559076feb91abf1c87c555c
Brandon Taft <btaftcan@gmail.com>
2023-11-08 22:40:16 -0500

    deploy to ebonee

--- pages/home.js
+++ pages/home.js
@@ -231,15 +231,15 @@ function SelectionPage() {
             </div>
             <div className='leaders'>
 
-                <h1 className='home-score'>
+                <h1>
                     TOP 10
                 </h1>
                 {leaderboard.slice(0, 10).map((score, index) => (
-                    <div className='side-board' key={index}>
-                        <div className='home-score'>{index + 1}.</div>
+                    <li className='side-board' key={index}>
+                        <div className='home-place'>{index + 1}.</div>
                         <div className="home-name">{score.username}</div>
                         <div className="home-score">{score.score}</div>
-                    </div>
+                    </li>
                 )
                 )}
             </div>
